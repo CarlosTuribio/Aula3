17c69d1a9169e208a10db52d2a863508482ec189
Nome <seu_email>
2020-02-21 21:48:44 -0300

    f

--- Projeto_Noticias/app_noticias/models.py
+++ Projeto_Noticias/app_noticias/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Noticia (models.Model):
-    conteudo = models.TextField()
+class Noticia(models.Model):
+    conteudo =  models.TextField()
+    autor = models.CharField('autor', max_length=128 , blank=True, null=True)
+    titulo = models.CharField('título', max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return self.conteudo
+
+    class Meta:
+        verbose_name = "Notícia"
+        verbose_name_plural = "Notícias"
+
+    
+    
+
